f3a71b9d3b6b4ac039d321943512bd84d945de0b
yuli-r <iuliana.rotaru234@gmail.com>
2023-05-22 22:51:38 +0100

    Set up repository

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# weather-app
+# Weather App
+## Introduction
+
+## Preview
+
+<!-- [![To Do List](./src/assets/demo.png)](https://yuliana-r.github.io/weather-app/) -->
+
+## Media & assets credits:
+
+1. Flaticon (iconixar): https://www.flaticon.com/free-icons/sun
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|webp|svg|ico)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name]-[hash][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/assets/icon.ico',
+      filename: 'index.html',
+      inject: true,
+    }),
+  ],
+};
