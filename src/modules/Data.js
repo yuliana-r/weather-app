108b4a2239393691fd49483825d604abd67b47c6
yuli-r <iuliana.rotaru234@gmail.com>
2023-10-24 14:52:08 +0100

    Update API key and secure with dotenv-webpack

--- src/modules/Data.js
+++ src/modules/Data.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-alert */
-import format from 'date-fns/format';
+import format from "date-fns/format";
 
 export default class Weather {
   static city;
@@ -13,23 +13,28 @@ export default class Weather {
   static current;
 
   static async getData(city) {
-    const apiKey = 'MLRZJ663MCNFCJ4UZHHPBGXWK';
+    const apiKey = process.env.KEY;
     const response = await fetch(
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/next7days?unitGroup=metric&include=days%2Ccurrent&iconSet=icons2&key=${apiKey}&contentType=json`,
-      { mode: 'cors' },
+      { mode: "cors" }
     );
 
     const data = await response.json();
 
     if (response.ok) {
-      const split = await data.resolvedAddress.indexOf(',');
-      this.city = await data.resolvedAddress.includes(',') ? data.resolvedAddress.slice(0, split) : data.resolvedAddress;
+      const split = await data.resolvedAddress.indexOf(",");
+      this.city = (await data.resolvedAddress.includes(","))
+        ? data.resolvedAddress.slice(0, split)
+        : data.resolvedAddress;
       this.country = await data.resolvedAddress.slice(split + 1).trim();
-      this.time = await format(new Date(`${data.days[0].datetime} ${data.currentConditions.datetime}`), 'eeee, do MMM \'\'yy, h:mm aaa');
+      this.time = await format(
+        new Date(`${data.days[0].datetime} ${data.currentConditions.datetime}`),
+        "eeee, do MMM ''yy, h:mm aaa"
+      );
       this.current = await data.currentConditions;
       this.weekForecast = await data.days;
     } else {
-      alert('Location not found.');
+      alert("Location not found.");
     }
   }
 
@@ -53,7 +58,7 @@ export default class Weather {
 
     return [
       day.icon,
-      format(new Date(day.datetime), 'eee'),
+      format(new Date(day.datetime), "eee"),
       Math.round(day.tempmax),
       Math.round(day.tempmin),
     ];
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,43 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  mode: "production",
+  entry: "./src/index.js",
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.html$/i,
-        use: 'html-loader',
+        use: "html-loader",
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpg|jpeg|gif|webp|svg|ico)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'assets/[name]-[hash][ext]',
+          filename: "assets/[name]-[hash][ext]",
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
+      template: "./src/index.html",
+      filename: "index.html",
       inject: true,
     }),
+    new Dotenv(),
   ],
 };
